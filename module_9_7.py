049ea9da026dea2f0ba1b02a09e861b762968f93
IgYozh <yozhmake@gmail.com>
2024-11-27 13:32:36 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,21 @@
+# #Домашнее задание по теме "Декораторы"
+
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+        sum_ = func(*args, **kwargs)
+        is_prime = True
+        for i in range(2, sum_):
+            if sum_ % i == 0:
+                is_prime = False
+        if is_prime == True:
+            return (f'Простое\n{sum_}')
+        else:
+            return (f'Составное\n{sum_}')
+    return wrapper
+@is_prime
+def sum_three(*value):
+    sum_ = sum(value)
+    return sum_
+
+result = sum_three(2, 3, 6)
+print(result)
