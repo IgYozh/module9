8d755df52dde05d2fb95fe7af521e716241c9fad
IgYozh <yozhmake@gmail.com>
2024-11-27 12:14:18 +0300

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,12 @@
+#Домашнее задание по теме "Генераторы"
+
+def all_variants(text):
+    for i in range(len(text)):
+        for l in range(len(text)-i):
+            yield text[l:l+i+1]
+
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
